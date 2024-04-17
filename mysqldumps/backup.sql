e6f98b6da27b5403d6f943ee3f5fc46fa7954ed5
Alexander <stwork87@gmail.com>
2024-04-17 14:10:33 +0300

    start3

--- mysqldumps/backup.sql
+++ mysqldumps/backup.sql
@@ -203,8 +203,8 @@ CREATE TABLE `wp_options` (
 --
 
 INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES
-(1, 'siteurl', 'http://ec2-54-91-253-138.compute-1.amazonaws.com', 'yes'),
-(2, 'home', 'http://ec2-54-91-253-138.compute-1.amazonaws.com', 'yes'),
+(1, 'siteurl', 'http://ec2-54-167-235-116.compute-1.amazonaws.com', 'yes'),
+(2, 'home', 'http://ec2-54-167-235-116.compute-1.amazonaws.com', 'yes'),
 (3, 'blogname', 'Bible Baptist Church', 'yes'),
 (4, 'blogdescription', '', 'yes'),
 (5, 'users_can_register', '0', 'yes'),
